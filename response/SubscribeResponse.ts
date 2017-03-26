f45f5cf71a7f4b79bacef391c0264dc212b285c1
Papp Gergely <gergely.papp@itworks.hu>
2017-03-26 21:04:28 +0200

    Added content to subscription

--- response/SubscribeResponse.ts
+++ response/SubscribeResponse.ts
@@ -1,11 +1,16 @@
 import {AbstractResponse} from "./AbstractResponse";
 import MapContainer from "../classes/MapContainer";
+import MapContent from "../classes/MapContent";
 export default class SubscribeResponse extends AbstractResponse {
     get mapContainer(): MapContainer {
         return this._mapContainer;
     }
 
-    constructor(private _mapContainer: MapContainer) {
+    get mapContent(): MapContent {
+        return this._mapContent;
+    }
+
+    constructor(private _mapContainer: MapContainer, private _mapContent: MapContent) {
         super();
     }
 }
